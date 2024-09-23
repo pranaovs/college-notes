258ab0c9a34da15ab62ae7bcc4c4218900f9e26a
Pranaov <148376495+pranaovs@users.noreply.github.com>
2024-09-23 17:48:46 +0530

    CS1001(assignment): create program to convert data from one unit to another

---
+++ Semester-1/CS1001-Programming_in_C/1-C_Programming_Basics/assignments/4-Decision_Making_and_Branching_2/3-Internet_plan/3_2-Internet_plan.c
@@ -0,0 +1,58 @@
+// Simple program to convert data from one unit to another
+// Promarily to be used with ISP data calculation
+
+#include <stdio.h>
+
+int main() {
+  float input;
+  char unit;
+  float KBps, Kbps, GBps, Gbps, MBps, Mbps, Bps, bps;
+
+  printf("Enter your data plan in the format <number><multiplier>Bps\nExample: "
+         "If your plan is 50MBps, enter 50M. Note: Omit Bps in your input)\n");
+
+  scanf("%f%c", &input, &unit);
+
+  if (unit == 'K' || unit == 'k') {
+    GBps = input / 1048576;
+    Gbps = GBps * 8;
+    MBps = input / 1024;
+    Mbps = MBps * 8;
+    KBps = input * 1;
+    Kbps = KBps * 8;
+    Bps = input * 1024;
+    bps = Bps * 8;
+  } else if (unit == 'M' || unit == 'm') {
+    GBps = input / 1024;
+    Gbps = GBps * 8;
+    MBps = input * 1;
+    Mbps = MBps * 8;
+    KBps = input * 1024;
+    Kbps = KBps * 8;
+    Bps = input * 1024 * 1024;
+    bps = input * 1024 * 1024 * 8;
+  } else if (unit == 'G' || unit == 'g') {
+    GBps = input * 1;
+    Gbps = GBps * 8;
+    MBps = input * 1024;
+    Mbps = MBps * 8;
+    KBps = input * 1048576;
+    Kbps = KBps * 8;
+    Bps = input * 1024 * 1024 * 1024;
+    bps = Bps * 8;
+  } else {
+    printf("Invalid input\n");
+    return 22; // Exit code for invalid argument
+  }
+
+  printf("Gigabytes per second (GBps):%.0f\n", GBps);
+  printf("Gigabits per second (Gbps):%.0f\n", Gbps);
+  printf("Megabytes per second (MBps):%.0f\n", MBps);
+  printf("Megabits per second (Mbps):%.0f\n", Mbps);
+  printf("Kilobytes per second (KBps): %.0f\n", KBps);
+  printf("Kilobits per second (Kbps): %.0f\n", Kbps);
+  printf("Bytes per second (Bps):%.0f\n", Bps);
+  printf("Bits per second (bps):%.0f\n", bps);
+
+  return 0;
+}
