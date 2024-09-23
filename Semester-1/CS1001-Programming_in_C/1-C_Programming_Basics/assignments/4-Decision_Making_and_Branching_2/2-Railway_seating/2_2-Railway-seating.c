cb356e56311d959dd0c9cdd6a150bfa585d8fe38
Pranaov <148376495+pranaovs@users.noreply.github.com>
2024-09-23 17:03:22 +0530

    CS1001(assignment): create code to find train seat berth given seat number

---
+++ Semester-1/CS1001-Programming_in_C/1-C_Programming_Basics/assignments/4-Decision_Making_and_Branching_2/2-Railway_seating/2_2-Railway-seating.c
@@ -0,0 +1,33 @@
+// Program to find out berth in train when seat number is provided
+
+#include <stdio.h>
+
+int main() {
+  int seat;
+  printf("Enter the seat number: ");
+  scanf("%d", &seat);
+
+  if (seat > 80 || seat <= 0) {
+    printf("Seat number %d is invalid. Train has only 80 seats\n", seat);
+    return 22; // Exit with Invalid argument error code
+  }
+
+  else if (seat % 8 == 0) {
+    printf("The seat assigned to %d is: SIDE UPPER BERTH\n", seat);
+  } else if (seat % 8 == 7) {
+    printf("The seat assigned to %d is: SIDE LOWER BERTH\n", seat);
+  } else if (seat % 8 == 6) {
+    printf("The seat assigned to %d is: UPPER BERTH\n", seat);
+  } else if (seat % 8 == 5) {
+    printf("The seat assigned to %d is: MIDDLE BERTH\n", seat);
+  } else if (seat % 8 == 4) {
+    printf("The seat assigned to %d is: LOWER BERTH\n", seat);
+  } else if (seat % 8 == 3) {
+    printf("The seat assigned to %d is: UPPER BERTH\n", seat);
+  } else if (seat % 8 == 2) {
+    printf("The seat assigned to %d is: MIDDLE BERTH\n", seat);
+  } else if (seat % 8 == 1) {
+    printf("The seat assigned to %d is: LOWER BERTH\n", seat);
+  }
+  return 0;
+}
