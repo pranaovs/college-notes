4d98eebb79104460382400ca822d5b6532726cc0
Pranaov S <148376495+pranaovs@users.noreply.github.com>
2025-02-21 22:34:40 +0530

    CS1804T: add try catch blocks in test cases

--- Semester-2/CS1804T-Data_Structures_Lab/Common/test.cpp
+++ Semester-2/CS1804T-Data_Structures_Lab/Common/test.cpp
@@ -1,12 +1,19 @@
+#include "CDLList.cpp"
+#include "DLList.cpp"
 #include "LList.cpp"
 
+using namespace std;
 int main(int argc, char *argv[]) {
-  LList list;
+  CDLList list;
 
   // Insertion and deletion at the beginning
   list.insertbeg(0);
   list.deletebeg();
-  list.deletebeg();
+  try {
+    list.deletebeg();
+  } catch (const char *msg) {
+    cout << msg << endl;
+  }
   list.insertbeg(0);
   list.deletebeg(5);
   list.deletebeg();
@@ -14,7 +21,11 @@ int main(int argc, char *argv[]) {
   // Insertion and deletion at the end
   list.insertend(1);
   list.deleteend();
-  list.deleteend();
+  try {
+    list.deleteend();
+  } catch (const char *msg) {
+    cout << msg << endl;
+  }
   list.insertend(1);
   list.deleteend(5);
   list.deleteend();
@@ -25,30 +36,111 @@ int main(int argc, char *argv[]) {
   list.insertend(1);
   list.deletebeg();
 
-  // Deleting empty list
-  list.deletebeg();
-  list.deleteend();
-  list.deletebeg(5);
-  list.deleteend(5);
-  list.deletepos(5);
-  list.deletepos(0);
-  list.deletepos(list.size);
+  try {
+    list.deletebeg();
+  } catch (const char *msg) {
+    cout << msg << endl;
+  }
+
+  try {
+    list.deleteend();
+  } catch (const char *msg) {
+    cout << msg << endl;
+  }
+
+  try {
+    list.deletebeg(5);
+  } catch (const char *msg) {
+    cout << msg << endl;
+  }
+
+  try {
+    list.deleteend(5);
+  } catch (const char *msg) {
+    cout << msg << endl;
+  }
+
+  try {
+    list.deletepos(5);
+  } catch (const char *msg) {
+    cout << msg << endl;
+  }
+
+  try {
+    list.deletepos(0);
+  } catch (const char *msg) {
+    cout << msg << endl;
+  }
+
+  try {
+    list.deletepos(list.size);
+  } catch (const char *msg) {
+    cout << msg << endl;
+  }
 
   // Insertion and deletion at position
-  list.insertpos(0, 0);
-  list.insertpos(1, 1);
-  list.insertpos(2, 2);
-  list.deletepos(1);
-  list.deletepos(5);
-  list.insertpos(12, 12);
-  list.deletepos(12);
-  list.insertpos(23, list.size);
-  list.deletepos(list.size);
+  try {
+    list.insertpos(0, 0);
+  } catch (const char *msg) {
+    cout << msg << endl;
+  }
+
+  try {
+    list.insertpos(1, 1);
+  } catch (const char *msg) {
+    cout << msg << endl;
+  }
+
+  try {
+    list.insertpos(2, 2);
+  } catch (const char *msg) {
+    cout << msg << endl;
+  }
+
+  try {
+    list.deletepos(1);
+  } catch (const char *msg) {
+    cout << msg << endl;
+  }
+
+  try {
+    list.deletepos(5);
+  } catch (const char *msg) {
+    cout << msg << endl;
+  }
+
+  try {
+    list.insertpos(12, 12);
+  } catch (const char *msg) {
+    cout << msg << endl;
+  }
+
+  try {
+    list.deletepos(12);
+  } catch (const char *msg) {
+    cout << msg << endl;
+  }
+
+  try {
+    list.insertpos(23, list.size);
+  } catch (const char *msg) {
+    cout << msg << endl;
+  }
+
+  try {
+    list.deletepos(list.size);
+  } catch (const char *msg) {
+    cout << msg << endl;
+  }
+
+  // Array insertions
+  int arr[] = {0, 1, 2, 3, 4, 5};
+  list.insertbeg(arr, 6);
+  list.insertend(arr, 6);
 
   // Searching
   list.search(0);
   list.search(1);
-  list.deletebeg(list.size);
   list.search(0); // Search in empty list
 
   // Displaying the list
