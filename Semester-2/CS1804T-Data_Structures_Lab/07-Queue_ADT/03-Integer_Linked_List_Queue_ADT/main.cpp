472acc4ea5dbe24dae87295f81792265f624f7e7
Pranaov S <148376495+pranaovs@users.noreply.github.com>
2025-04-29 14:40:45 +0530

    CS1804T: change filetypes from dos to unix

--- Semester-2/CS1804T-Data_Structures_Lab/07-Queue_ADT/03-Integer_Linked_List_Queue_ADT/main.cpp
+++ Semester-2/CS1804T-Data_Structures_Lab/07-Queue_ADT/03-Integer_Linked_List_Queue_ADT/main.cpp
@@ -1,93 +1,80 @@
 #include <iostream>
 using namespace std;
 
-class queue
-{
-    struct node
-    {
-        int data;
-        node *next;
-    };
-    node *front, *rear;
-    
-    public:
-    
-    queue()
-    {
-        front = NULL;
-        rear = NULL;
-    }
+class queue {
+  struct node {
+    int data;
+    node *next;
+  };
+  node *front, *rear;
 
-    void enqueue(int x)
-    {
-        node *temp = new node;
-        temp->data = x;
-        temp->next = NULL;
-        if(front == NULL)
-        {
-            front = temp;
-            rear = temp;
-            return;
-        }
-        rear->next = temp;
-        rear = temp;
-    }
+public:
+  queue() {
+    front = NULL;
+    rear = NULL;
+  }
 
-    void dequeue()
-    {
-        if(front == NULL)
-        {
-            cout << "Queue is empty" << endl;
-            return;
-        }
-        node *temp = front;
-        front = front->next;
-        delete temp;
+  void enqueue(int x) {
+    node *temp = new node;
+    temp->data = x;
+    temp->next = NULL;
+    if (front == NULL) {
+      front = temp;
+      rear = temp;
+      return;
     }
+    rear->next = temp;
+    rear = temp;
+  }
 
-    void peek()
-    {
-        if(front == NULL)
-        {
-            cout << "Queue is empty" << endl;
-            return;
-        }
-        cout << front->data << endl;
+  void dequeue() {
+    if (front == NULL) {
+      cout << "Queue is empty" << endl;
+      return;
     }
+    node *temp = front;
+    front = front->next;
+    delete temp;
+  }
+
+  void peek() {
+    if (front == NULL) {
+      cout << "Queue is empty" << endl;
+      return;
+    }
+    cout << front->data << endl;
+  }
 };
 
-int main()
-{
-    int exit = 0;
-    queue q;
-    while(exit == 0)
-    {
-        cout << "1. Enqueue" << endl;
-        cout << "2. Dequeue" << endl;
-        cout << "3. Peek" << endl;
-        cout << "4. Exit" << endl;
-        int choice;
-        cin >> choice;
-        switch(choice)
-        {
-            case 1:
-                cout << "Enter the element to enqueue: ";
-                int x;
-                cin >> x;
-                q.enqueue(x);
-                break;
-            case 2:
-                q.dequeue();
-                break;
-            case 3:
-                q.peek();
-                break;
-            case 4:
-                exit = 1;
-                break;
-            default:
-                cout << "Invalid choice" << endl;
-        }
+int main() {
+  int exit = 0;
+  queue q;
+  while (exit == 0) {
+    cout << "1. Enqueue" << endl;
+    cout << "2. Dequeue" << endl;
+    cout << "3. Peek" << endl;
+    cout << "4. Exit" << endl;
+    int choice;
+    cin >> choice;
+    switch (choice) {
+    case 1:
+      cout << "Enter the element to enqueue: ";
+      int x;
+      cin >> x;
+      q.enqueue(x);
+      break;
+    case 2:
+      q.dequeue();
+      break;
+    case 3:
+      q.peek();
+      break;
+    case 4:
+      exit = 1;
+      break;
+    default:
+      cout << "Invalid choice" << endl;
     }
-    return 0;
+  }
+  return 0;
 }
--- Semester-2/CS1804T-Data_Structures_Lab/07-Queue_ADT/04-String_Plus_Symbol/main.cpp
+++ Semester-2/CS1804T-Data_Structures_Lab/07-Queue_ADT/04-String_Plus_Symbol/main.cpp
@@ -1,33 +1,26 @@
-#include <iostream>
 #include "stack-sll.h"
+#include <iostream>
 using namespace std;
 
-void removePlus(string s)
-{
-    stack s1;
-    for(int i=0;i<s.length();i++)
-    {
-        if(s[i]=='+')
-        {
-            s1.pop();
-        }
-        else
-        {
-            s1.push(s[i]);
-        }
+void removePlus(string s) {
+  stack s1;
+  for (int i = 0; i < s.length(); i++) {
+    if (s[i] == '+') {
+      s1.pop();
+    } else {
+      s1.push(s[i]);
     }
-    while(s1.head!=NULL)
-    {
-        cout<<s1.head->data;
-        s1.head=s1.head->next;
-    }
+  }
+  while (s1.head != NULL) {
+    cout << s1.head->data;
+    s1.head = s1.head->next;
+  }
 }
 
-int main()
-{
-    string s;
-    cout<<"Enter a string: ";
-    cin>>s;
-    removePlus(s);
-    return 0;
+int main() {
+  string s;
+  cout << "Enter a string: ";
+  cin >> s;
+  removePlus(s);
+  return 0;
 }
